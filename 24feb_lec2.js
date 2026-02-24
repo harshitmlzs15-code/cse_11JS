5c79fbc8b775d2cdc56e9787a783bc99244508e4
harshitmlzs15-code <harshitmlzs15@gmail.com>
2026-02-24 10:30:11 +0530

    24feb

---
+++ 24feb_lec2.js
@@ -0,0 +1,34 @@
+//operators
+ const a = 10;
+ const b = 13;
+ let c = a + b;
+ console.log(c);
+
+ {
+const a = 10;
+const b = 13;
+let c = a * b;
+console.log(c);
+ }
+
+  {
+const a = 100;
+const b = 12;
+let c = a / b;
+console.log(c);
+ }
+
+  {
+const a = 190;
+const b = 20;
+let c = a % b;
+console.log(c);
+ }
+
+
+{
+const a = 199;
+const b = 20;
+let c = a ==b;
+console.log(c);
+ }
